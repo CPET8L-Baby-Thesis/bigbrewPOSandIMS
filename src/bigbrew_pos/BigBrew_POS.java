6cf8771b3e45669ff83c780ce4c8a37b1a427a12
Joey Boy Mission <joeyboy.mission@tup.edu.ph>
2023-05-18 16:50:56 +0800

    This is the Initial Code

---
+++ src/bigbrew_pos/BigBrew_POS.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bigbrew_pos;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.time.format.DateTimeFormatter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+/**
+ *
+ * @author JOIBOI
+ */
+public class BigBrew_POS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new userAuth();
+        new pos();
+    }
+    
+}
